4d3f4b425cc41404797768920b93bd77344ef49b
Emil Aune Jakobsen <emilajakobsen@gmail.com>
2023-04-12 10:15:33 +0200

    div + la til plot_eigs

--- dynpssimpy/ps_models/k2a_base_script.py
+++ dynpssimpy/ps_models/k2a_base_script.py
@@ -58,6 +58,10 @@ if __name__ == '__main__':
     print(' ')
     print('Voltage angles     (rad) = ', v_bus_angle)
     print(' ')
+    # ** adding angles in degrees
+    print('Voltage angles     (deg) = ', v_bus_angle*180/np.pi)
+    print(' ')
+    # ****
     print('Voltage magnitudes  (kV) = ', v_bus_mag*[20, 20, 20, 20, 230, 230, 230, 230, 230, 230, 230])
     print(' ')
     # print(ps.v_n)
@@ -74,15 +78,15 @@ if __name__ == '__main__':
         # print(t)
         #v_bus_full = ps.red_to_full.dot(ps.v_red)
         # Simulate short circuit
-        if 1 < t < 1.1:
+        """if 1 < t < 1.1:
           ps.y_bus_red_mod[7, 7] = 10000
         else:
-           ps.y_bus_red_mod[7, 7] = 0
-        # simulate a load change
-        # if 2 < t < 4:
-        #    ps.y_bus_red_mod[2, 2] = 0.1
-        # else:
-        #   ps.y_bus_red_mod[2, 2] = 0
+           ps.y_bus_red_mod[7, 7] = 0"""
+        # simulate a short circuit at bus 5 - preliminaries
+        if 1 < t < 1.05:
+            ps.y_bus_red_mod[4, 4] = 10000
+        else:
+            ps.y_bus_red_mod[4, 4] = 0
         # Simulate next step
         result = sol.step()
         x = sol.y
@@ -99,6 +103,15 @@ if __name__ == '__main__':
 
     print('Simulation completed in {:.2f} seconds.'.format(time.time() - t_0))
 
+    # ** Obtaining power flows
+    # ps.power_flow(True)
+    print(ps.s_0)
+
+    # P15 = (v_bus_mag[0]-v_bus_mag[4]) * ps.y_bus_red_full[0][4]
+    # print(P15)
+
+    # ****
+
     # Convert dict to pandas dataframe
     index = pd.MultiIndex.from_tuples(result_dict)
     result = pd.DataFrame(result_dict, columns=index)
@@ -108,11 +121,14 @@ if __name__ == '__main__':
     fig.suptitle('Generator speed, angle and electric power')
     ax[0].plot(result[('Global', 't')], result.xs(key='speed', axis='columns', level=1))
     ax[0].set_ylabel('Speed (p.u.)')
+    ax[0].legend(['G1', 'G2', 'G3', 'G4'])
     ax[1].plot(result[('Global', 't')], result.xs(key='angle', axis='columns', level=1))
     ax[1].set_ylabel('Angle (rad.)')
+    ax[1].legend(['G1', 'G2', 'G3', 'G4'])
     ax[2].plot(result[('Global', 't')], np.array(P_e_stored)/[900, 900, 900, 900])
     ax[2].set_ylabel('Power (p.u.)')
     ax[2].set_xlabel('time (s)')
+    ax[2].legend(['G1', 'G2', 'G3', 'G4'])
 
     plt.figure()
     plt.plot(result[('Global', 't')], np.array(E_f_stored))
@@ -123,5 +139,11 @@ if __name__ == '__main__':
     plt.plot(result[('Global', 't')], np.array(I_stored))
     plt.xlabel('time (s)')
     plt.ylabel('I_8-9 (magnitude p.u.)')
+
+    # ** Plot for 1a)
+    """plt.figure()
+    plt.plot(result[('Global', 't')], result.xs(key='V_t_abs', axis='columns', level=1))"""
+
+    # ****
 
     plt.show()
